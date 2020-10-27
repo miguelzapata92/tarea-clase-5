5d054249c537e8cebeef40ecef665353cc9cf744
miguelzapata92 <71832311+miguelzapata92@users.noreply.github.com>
2020-10-27 16:28:06 -0300

    Revert "Initial commit"
    This reverts commit 0a3dfe589ee0083f366f24f7c1a2c2e7c3a1d742.

---
+++ tarea-2.js
@@ -0,0 +1,25 @@
+const $botonCargar = document.querySelector("#cargar-formulario")
+
+const $datosCargados = document.querySelector("datos-cargados")
+
+const $saludo = document.querySelector("saludo-bienvenida")
+
+
+
+$botonCargar.onclick = function(event) {
+
+    event.preventDefault()
+    
+    const primerNombre =  document.querySelector("#primer-nombre").value
+
+    const segundoNombre = document.querySelector("#segundo-nombre").value   
+    
+    const apellido = document.querySelector("#apellido").value
+    
+    const edad = Number(document.querySelector("#edad-usuario")).value
+
+    $datosCargados.innerHTML = `Bienvenida ${n}`
+
+}
+
+
