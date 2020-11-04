a0beab81e8424bc253fecfb154d822dbcb683d31
miguelzapata92 <zptmiguel.92@gmail.com>
2020-11-04 18:59:26 -0300

    tarea-4
    publicación final

--- tarea-4/tarea-4.js
+++ tarea-4/tarea-4.js
@@ -12,3 +12,31 @@ function llenarArray($arrayLi) {
 
 llenarArray($arrayLi)
 console.log(arrayNumeros)
+
+let $numeroMasGrande = Math.max(...arrayNumeros)
+
+resultado1 = document.querySelector("#numero-grande")
+
+resultado1.innerText = $numeroMasGrande
+
+
+let $numeroChico = Math.min(...arrayNumeros)
+
+resultado2 = document.querySelector("#numero-chico")
+
+resultado2.innerText = $numeroChico
+
+totalDeNumeros = 0
+
+for(i = 0; i < arrayNumeros.length; i++) {
+    totalDeNumeros += arrayNumeros[i]
+}
+
+let promedio = totalDeNumeros/arrayNumeros.length
+
+resultado3 = document.querySelector("#promedio")
+
+resultado3.innerText = promedio
+
+
+
